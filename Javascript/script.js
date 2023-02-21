3cb38c1bbbd180c6adad352cf020dc9db5cf501b
Santiuwu1809 <aristizabalsantiago482@gmail.com>
2023-02-21 11:56:42 -0500

    modified:   Javascript/script.js 	modified:   index.html

---
+++ Javascript/script.js
@@ -0,0 +1,40 @@
+let input = document.querySelector("#searchInput");
+let userList = document.querySelector("#users");
+
+let users = [];
+
+window.addEventListener("DOMContentLoaded", async () => {
+  userList.innerHTML = "<h1>Loading</h1>";
+  const data = await loadUsers();
+  users = data.data;
+  renderUsers(users);
+});
+
+async function loadUsers() {
+  const response = await fetch(
+    "https://fakerapi.it/api/v1/users?_quantity=1000"
+  );
+  return await response.json();
+}
+
+input.addEventListener("keyup", (e) => {
+  const newUsers = users.filter((user) =>
+    `${user.firstname.toLowerCase()}${user.lastname.toLowerCase()}`.includes(
+      input.value.toLowerCase()
+    )
+  );
+  renderUsers(newUsers);
+});
+
+const createUserItem = (users) =>
+  users
+    .map(
+      (user) =>
+        `<li class="bg-zinc-800 hover:bg-zinc-700 hover:cursor-pointer">${user.firstname} ${user.lastname}</li>`
+    )
+    .join(" ");
+
+function renderUsers(users) {
+  const itemsString = createUserItem(users);
+  userList.innerHTML = itemsString;
+}
